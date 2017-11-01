069cf96bc6c31867d01589c4a600ee4d7265df06
31645720 <31645720@087939-P33S104.macklabs.br>
2017-11-01 09:56:41 -0200

    Inicialização do Projeto

--- src/app/App.java
+++ src/app/App.java
@@ -15,7 +15,8 @@ public class App {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int x = 10;
+        System.out.println("valor de x é igual a "+x);
     }
     
 }
